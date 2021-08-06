ffc3bf951cceda0828cd12fa69ff5574a587a25d
berii <86112927+AcaiBerii@users.noreply.github.com>
2021-08-06 23:26:24 +0000

    god its even uglier
    Haha

--- LunaGO.go
+++ LunaGO.go
@@ -3,7 +3,7 @@ package main
 // go run Luna.go TOKEN SERVERID [ur code is horrible]
 
 import (
-	"time"
+    "time"
     "bufio"
     "os"
     "fmt" // why use log? 
@@ -11,18 +11,17 @@ import (
 )
 
 func main() {
-	fmt.Println("[~] Welcome To Luna!")
+    fmt.Println("[~] Welcome To Luna!")
 	
-	token := os.Args[1]
-	guild := os.Args[2]
+    token := os.Args[1]
+    guild := os.Args[2]
 
-	BanAll(token, guild)
+    BanAll(token, guild)
 
-	fmt.Println("[~] Finished!")
+    fmt.Println("[~] Finished!")
 }
 
 func BanAll(token, guild string) {
-
     fmt.Println("[~] Loading IDs")
 
     file, err := os.Open("Members.txt")
@@ -44,29 +43,29 @@ func BanAll(token, guild string) {
 }
 
 func Send_Request(token, guild, user string) { // no need to add string besides at the end (duh)
-	url := fmt.Sprintf("https://discord.com/api/v8/guilds/%s/bans/%s", guild, user)
+    url := fmt.Sprintf("https://discord.com/api/v8/guilds/%s/bans/%s", guild, user)
+ 
+    req, err := http.NewRequest("PUT", url, nil)
+    if err != nil {
+        return
+    }
 
-	req, err := http.NewRequest("PUT", url, nil)
-	if err != nil {
-		return
-	}
+    req.Header.Set("Authorization", token)
 
-	req.Header.Set("Authorization", token)
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+	fmt.Println(err) // Print the error
+	return
+    }
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println(err) // Print the error
-		return
-	}
-
-	if resp.StatusCode == 204 {
-		fmt.Printf("Successfully Banned %s\n", user)
-		return // kill it
-	} 
-	if resp.StatusCode == 429 {
-		fmt.Println("[!] Ratelimited")
-		time.Sleep(1 * time.Second) // one second is more then enough
-		Send_Request(token, guild, user)
-	}
+    if resp.StatusCode == 204 {
+        fmt.Printf("Successfully Banned %s\n", user)
+        return // kill it
+    } 
+    if resp.StatusCode == 429 {
+        fmt.Println("[!] Ratelimited")
+        time.Sleep(1 * time.Second) // one second is more then enough
+        Send_Request(token, guild, user)
+    }
 
 }
